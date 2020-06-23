6d9dfde67e7422c25ceb47d83af75789ce86f442
TStupariu <stupariu.tudorf@gmail.com>
2020-06-23 14:09:57 +0300

    Finished the add photo screen

--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -3,6 +3,9 @@ import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text} from 'native-base';
 import ImagePicker from 'react-native-image-crop-picker';
 import {useNavigation} from '@react-navigation/native';
+import {colors} from '../styles/common';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {ListDivider} from '../components';
 
 const IMAGE_OPTIONS = {
   compressImageMaxWidth: 1080,
@@ -38,11 +41,14 @@ const AddPostScreen = props => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.galleryContainer} onPress={openGallery}>
-        <Text style={styles.text}>Upload from gallery</Text>
-      </TouchableOpacity>
       <TouchableOpacity style={styles.cameraContainer} onPress={openCamera}>
+        <Icon name="camera-alt" size={100} color={colors.white} />
         <Text style={styles.text}>Take a picture</Text>
+        <View style={styles.divider} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.galleryContainer} onPress={openGallery}>
+        <Icon name="photo-library" size={100} color={colors.white} />
+        <Text style={styles.text}>Upload from gallery</Text>
       </TouchableOpacity>
     </View>
   );
@@ -55,17 +61,28 @@ const styles = StyleSheet.create({
   galleryContainer: {
     flex: 1 / 2,
     justifyContent: 'center',
-    backgroundColor: 'red',
+    alignItems: 'center',
+    backgroundColor: colors.blue,
   },
   cameraContainer: {
     flex: 1 / 2,
     justifyContent: 'center',
-    backgroundColor: 'green',
+    alignItems: 'center',
+    backgroundColor: colors.black,
   },
   text: {
     color: '#fff',
     textAlign: 'center',
-    fontSize: 30,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  divider: {
+    position: 'absolute',
+    bottom: 0,
+    width: '60%',
+    height: 1,
+    backgroundColor: colors.white,
   },
 });
 
--- styles/common.js
+++ styles/common.js
@@ -5,6 +5,7 @@ const colors = {
   lightGrey: '#8F8F8F',
   darkGrey: '#272727',
   black: '#000',
+  blue: '#001038',
 };
 
 export {colors};
